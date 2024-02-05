89748f9d9b96528866fc725e5088f1fe14b89012
Ali Shahid <shaeinst@gmail.com>
2024-02-06 00:15:44 +0545

    [feat] now user can config server and more..

---
+++ lua/override/mapping.lua
@@ -0,0 +1,4 @@
+return {
+	-- ["<Leader>f"] = { "<cmd>lua require('telescope.builtin').commands() <CR>", "Commands" },
+	-- th = { "<cmd>lua require('telescope.builtin').help_tags() <CR>", "Help tags" },
+}
