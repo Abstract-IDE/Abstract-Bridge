ea290bd4a5f8e312bc8c5473e4ad3d1d6445fa21
New Name <code@AlixShahid.com>
2024-04-20 22:33:29 +0400

    [override/none-ls] add support for user defined config

--- lua/override/none-ls.lua
+++ lua/override/none-ls.lua
@@ -1,25 +1,39 @@
 local M = {}
 
--- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
-M.formatting = {
-	["luaformatter"] = {
-		lua_format = {
-			command = "lua-format",
-			args = {
-				"--indent-width",
-				"1",
-				"--tab-width",
-				"4",
-				"--use-tab",
-				"--chop-down-table",
-				"--extra-sep-at-table-end",
-			},
-		},
-	},
-}
+M.setup = function(null, installed_packages)
+	local sources = {}
+	-- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+	-- local formatting = null.builtins.formatting
+	-- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/completion
+	-- local completion = null.builtins.completion
+	-- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+	-- local diagnostics = null.builtins.diagnostics
+	-- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/code_actions
+	-- local code_actions = null.builtins.code_actions
 
-M.diagnostics = {}
-M.code_actions = {}
-M.completion = {}
-M.hover = {}
+	-- for _, package in pairs(installed_packages) do
+	-- 	-- Extend sources list here
+	-- 	-- Examples:
+	-- 	if package == "stylua" then
+	-- 		sources[#sources + 1] = formatting.stylua.with({
+	-- 			command = "stylua",
+	-- 			args = {
+	-- 				"--search-parent-directories",
+	-- 				"--stdin-filepath",
+	-- 				"$FILENAME",
+	-- 				"-",
+	-- 			},
+	-- 		})
+	-- 	end
+	-- 	if package == "black" then
+	-- 		sources[#sources + 1] = formatting.black.with({
+	-- 			command = "black",
+	-- 			args = { "--quiet", "--fast", "-" },
+	-- 		})
+	-- 	end
+	-- end
+
+	return sources
+end
+
 return M
