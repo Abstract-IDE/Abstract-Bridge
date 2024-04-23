153c9e66937b9e195855f432d809fb19fbc104ee
Ali Shahid <shaeinst@gmail.com>
2024-04-23 16:59:06 +0400

    [configs/plugins] move colorscheme, status bar and winbar to template

--- lua/plugins/special.lua
+++ lua/plugins/special.lua
@@ -2,23 +2,36 @@
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 ─────────────────────────────────────────────────
 NOTE:
-This config file is special. You can use it to install any colorscheme you like and set the colorscheme.
-This will override the default colorscheme provided by Abstract.
-You can delete this file to use the Abstract default colorscheme.
+This is a special config file. You can change the colorscheme, winbar, and statusbar here.
 ─────────────────────────────────────────────────
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 --]]
 
 local spec = {
 	"Abstract-IDE/Abstract-cs",
+	dependencies = {
+		"Abstract-IDE/abstract-line",
+		{
+			"Abstract-IDE/abstract-winbar",
+			lazy = true,
+			event = { "BufRead" },
+			dependencies = { "SmiteshP/nvim-navic" },
+		},
+	},
 	lazy = false, -- make sure we load this during startup if it is your main colorscheme
 	priority = 1000, -- make sure to load this before all the other start plugins
 }
 
 spec.config = function()
-	pcall(vim.cmd, "colorscheme abscs")
-	-- theme name
-	vim.g.abscs_theme_name = "aqua"
+	-- color scheme
+	vim.cmd("colorscheme abscs")
+	vim.g.abscs_theme_name = "aqua" -- theme name
+
+	-- status line
+	require("abstract-line").setup()
+
+	-- winbar
+	require("abstract-winbar").setup({})
 end
 
 return spec
