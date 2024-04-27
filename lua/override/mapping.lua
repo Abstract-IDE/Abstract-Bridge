2e9b0191fdbc56956ce0b2cce73aa740f401257c
New Name <code@AlixShahid.com>
2024-04-27 22:24:07 +0400

    add comments

--- lua/override/mapping.lua
+++ lua/override/mapping.lua
@@ -1,3 +1,12 @@
+--[[
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+─────────────────────────────────────────────────
+https://github.com/folke/which-key.nvim
+We are using which-key.nvim for mapping.
+─────────────────────────────────────────────────
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+--]]
+
 return {
 	-- ["<Leader>f"] = { "<cmd>lua require('telescope.builtin').commands() <CR>", "Commands" },
 	-- th = { "<cmd>lua require('telescope.builtin').help_tags() <CR>", "Help tags" },
