bcb54d1424b989b4ae9604d6af19349ba7008082
New Name <code@AlixShahid.com>
2024-10-31 09:53:08 +0400

    [fix] hard coded installation path

--- init.lua
+++ init.lua
@@ -2,8 +2,7 @@
 -- WARNING: This snippet must be placed at the top of init.lua.
 -- ─────────────────────────────────────────────────
 if vim.g.ABSTRACT_LOADED == nil then
-	local abstract_path = "/home/sh4h1d/codeDNA/dev/Projects/neovim/Abstract"
-	-- local abstract_path = vim.fn.stdpath("data") .. "/Abstract"
+	local abstract_path = vim.fn.stdpath("data") .. "/Abstract"
 	if not (vim.uv or vim.loop).fs_stat(abstract_path) then
 		vim.fn.system({ "git", "clone", "https://github.com/Abstract-IDE/Abstract", "--branch=rewrite", abstract_path })
 	end
