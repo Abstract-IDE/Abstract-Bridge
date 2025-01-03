89e755059265d23c7bd7870acca657fbba5d768f
Ali Shahid <shaeinst@gmail.com>
2025-01-03 16:06:41 +0400

    [bridge] refactor for batter installation

--- init.lua
+++ init.lua
@@ -1,20 +1,17 @@
 -- ─────────────────────────────────────────────────
 -- WARNING: This snippet must be placed at the top of init.lua.
 -- ─────────────────────────────────────────────────
-if vim.g.ABSTRACT_LOADED == nil then
+if ABSTRACT == nil then
 	local abstract_path = vim.fn.stdpath("data") .. "/Abstract"
 	if not (vim.uv or vim.loop).fs_stat(abstract_path) then
 		vim.fn.system({ "git", "clone", "https://github.com/Abstract-IDE/Abstract", "--branch=rewrite", abstract_path })
 	end
 	vim.opt.rtp:prepend(abstract_path)
 	require("abstract").setup(abstract_path)
-end
-if vim.g.ABSTRACT_LOADED then
 	return
 end
 -- ─────────────────────────────────────────────────
 
-
 -- everything else goes below
-
 -- vim.g.mapleader = "|"
+-- vim.opt.relativenumber=false
