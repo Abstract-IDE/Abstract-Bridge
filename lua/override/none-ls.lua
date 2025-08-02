0d41d06435479ace6c48844d186e7ce92be7c7a8
Ali Shahid <code@AlixShahid.com>
2025-08-02 22:41:51 +0400

    [special] update with abstract

--- lua/override/none-ls.lua
+++ lua/override/none-ls.lua
@@ -1,37 +1,30 @@
 local M = {}
 
-M.setup = function(null, installed_packages)
-	local sources = {}
-	-- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+M.sources = function(nullls)
+	-- -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 	-- local formatting = null.builtins.formatting
-	-- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/completion
+	-- -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/completion
 	-- local completion = null.builtins.completion
-	-- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+	-- -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 	-- local diagnostics = null.builtins.diagnostics
-	-- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/code_actions
+	-- -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/code_actions
 	-- local code_actions = null.builtins.code_actions
 
-	-- for _, package in pairs(installed_packages) do
-	-- 	-- Extend sources list here
-	-- 	-- Examples:
-	-- 	if package == "stylua" then
-	-- 		sources[#sources + 1] = formatting.stylua.with({
-	-- 			command = "stylua",
-	-- 			args = {
-	-- 				"--search-parent-directories",
-	-- 				"--stdin-filepath",
-	-- 				"$FILENAME",
-	-- 				"-",
-	-- 			},
-	-- 		})
-	-- 	end
-	-- 	if package == "black" then
-	-- 		sources[#sources + 1] = formatting.black.with({
-	-- 			command = "black",
-	-- 			args = { "--quiet", "--fast", "-" },
-	-- 		})
-	-- 	end
-	-- end
+	local sources = {
+		-- formatting.stylua.with({
+		-- 	command = "stylua",
+		-- 	args = {
+		-- 		"--search-parent-directories",
+		-- 		"--stdin-filepath",
+		-- 		"$FILENAME",
+		-- 		"-",
+		-- 	},
+		-- }),
+		-- formatting.black.with({
+		-- 	command = "black",
+		-- 	args = { "--quiet", "--fast", "-" },
+		-- }),
+	}
 
 	return sources
 end
