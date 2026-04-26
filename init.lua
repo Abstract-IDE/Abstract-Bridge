b790706e87fbde295d1c50d4795cb8687f6d51f5
Ali Shahid <code@AlixShahid.com>
2026-04-26 16:06:25 +0400

    feat: minor updates

--- init.lua
+++ init.lua
@@ -2,11 +2,11 @@
 --- WARNING: This snippet must be placed at the top of init.lua.
 --- ─────────────────────────────────────────────────
 local data = vim.fn.stdpath("data")
-local root = data .. "/abstract"
+local repo = data .. "/abstract"
 
-if vim.fn.isdirectory(root) == 0 then
+if vim.fn.isdirectory(repo) == 0 then
     vim.fn.system({ "git", "clone", "--depth", "1", "-b", "rust-rewrite", "https://github.com/Abstract-IDE/Abstract.git",
-        root })
+        repo })
 end
-dofile(root .. "/code/lua/init.lua")(root, data)
+dofile(repo .. "/code/lua/init.lua")(data, repo)
 --- ─────────────────────────────────────────────────
